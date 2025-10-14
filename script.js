d96561714c1ddf436f161c5b9a8f49b47837003c
wulffKaue <seu-email-cadastrado-no-github@example.com>
2025-10-14 19:27:23 -0300

    Aplicando JS pra mudar de capítulo e mudar texto do capítulo atual.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const Audio = document.getElementById("id-capitulo");
+const ButtonNextCap = document.getElementById("proximo");
+const ButtonPreviousCap = document.getElementById("anterior");
 const PlayButton = document.getElementById("play-pause");
+const CapH1 = document.getElementById("capitulo");
+const AmountCaps = 10;
 
 let AudioPS = false;
+let CapCurrent = 1;
 
 function PlayAudio() {
   Audio.play();
@@ -23,4 +28,30 @@ function PlayOrPause() {
   }
 }
 
+function NextCap() {
+  StopAudio();
+  if (CapCurrent < AmountCaps) {
+    CapCurrent = CapCurrent + 1;
+  } else {
+    CapCurrent = 1;
+  }
+  Audio.src = "./audios/" + CapCurrent + ".mp3";
+  CapH1.innerText = "Capítulo " + CapCurrent;
+  PlayButton.classList.remove("tocando");
+}
+
+function PrevCap() {
+  StopAudio();
+  if (CapCurrent === 1) {
+    CapCurrent = AmountCaps;
+  } else {
+    CapCurrent = AmountCaps - 1;
+  }
+  Audio.src = "./audios/" + CapCurrent + ".mp3";
+  CapH1.innerText = "Capítulo " + CapCurrent;
+  PlayButton.classList.remove("tocando");
+}
+
 PlayButton.addEventListener("click", PlayOrPause);
+ButtonNextCap.addEventListener("click", NextCap);
+ButtonPreviousCap.addEventListener("click", PrevCap);
